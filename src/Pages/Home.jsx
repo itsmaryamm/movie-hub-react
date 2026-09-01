41bf59187d6756fadefc3284b3a5d0e04af1322b
itsmaryamm <maryameshal876@gmail.com>
2026-09-01 23:26:47 +0500

    WIP: Movie App

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,24 +1,46 @@
 import React from 'react'
+import MovieCard from '../Components/MovieCard'
+import {useState} from 'react'
 
 const Home = () => {
-    const movies = [
-        {id: 1, title: 'Movie 1', release_date: '2022-01-01'},
-        {id: 2, title: 'Movie 2', release_date: '2023-01-01'},
-        {id: 3, title: 'Movie 3', release_date: '2024-01-01'},
-        ]
 
-  return (
-    <div className="home">
-      <div className="movie-grid">
-        {movies.map(movie => (
-          <div key={movie.id} className="movie">
-            <h3>{movie.title}</h3>
-            <p>Release Date: {movie.release_date}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+    const [searchTerm, setSearchTerm] = React.useState('')
+
+    const movies = [
+        { id: 1, title: 'John Doe', release_date: '2022-01-01' },
+        { id: 2, title: 'Jane Smith', release_date: '2023-01-01' },
+        { id: 3, title: 'Bob Johnson', release_date: '2024-01-01' },
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchTerm);
+        setSearchTerm('');
+     }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className='search-form'>
+                <input 
+                    type="text" 
+                    placeholder='Search for a movie...' 
+                    className='search-input' 
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                    />
+
+                <button type="submit" className='search-button'>Search</button>
+            </form>
+
+            <div className="movie-grid">
+                {movies.map(movie => 
+                    movie.title.toLowerCase().startsWith(searchTerm) && (
+                        <MovieCard key={movie.id} movie={movie} />
+                    )
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Home
